727a88a5190ce9815740f0431ab190d9f38acfb3
iutkarsh077 <utkarshsingh132002@gmail.com>
2024-02-09 04:43:44 +0530

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import myimg from "./assets/myimg.jpg";
 import code from "./assets/code.png";
 import design from "./assets/design.png";
-import Netflix from './assets/port-netflix.png'
+import Netflix from './assets/port-netflix.png';
 import Textspeech from "./assets/Text to speech - Google Chrome 19-10-2023 13_40_52.png";
 import aeroplane from "./assets/Text to speech - Google Chrome 19-10-2023 13_41_56.png";
 import sidhu from "./assets/Text to speech - Google Chrome 19-10-2023 13_42_37.png";
